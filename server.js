daac9447115da518fdc060d64fab2c9e1582c14f
dalouousena-dev <dalouousena@gmail.com>
2026-04-23 20:29:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -235,6 +235,8 @@ app.get('/api/plans', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`✓ Server running on port ${PORT}`);
 });
